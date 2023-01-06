cb4346ee96e47dc66dccee49a95e3135bc7b6d2d
valdirapn <valdiraires.pn@gmail.com>
2023-01-06 01:08:52 -0300

    implement home ui xml

--- app/src/main/java/com/vpn/monet/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/vpn/monet/presentation/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.vpn.monet
+package com.vpn.monet.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vpn.monet.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
