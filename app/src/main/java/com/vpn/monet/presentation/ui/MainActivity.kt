f4d5e7dd05d25743313fd5603941961b0a43e89e
valdirapn <valdiraires.pn@gmail.com>
2023-01-06 10:00:19 -0300

    setup BottomNavigation

--- app/src/main/java/com/vpn/monet/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/vpn/monet/presentation/ui/MainActivity.kt
@@ -2,11 +2,27 @@ package com.vpn.monet.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.vpn.monet.R
+import com.vpn.monet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupBottomNavigation()
+    }
+
+    private fun setupBottomNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNav.setupWithNavController(navController)
     }
 }
