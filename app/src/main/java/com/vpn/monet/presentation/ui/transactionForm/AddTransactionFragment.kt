03c9f0d5d368040244e55e8ebfb6c7c5dcaf8da7
valdirapn <valdiraires.pn@gmail.com>
2023-01-07 08:12:00 -0300

    move fragments to its packages

---
+++ app/src/main/java/com/vpn/monet/presentation/ui/transactionForm/AddTransactionFragment.kt
@@ -0,0 +1,20 @@
+package com.vpn.monet.presentation.ui.transactionForm
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.vpn.monet.R
+
+class AddTransactionFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_transaction, container, false)
+    }
+
+}
